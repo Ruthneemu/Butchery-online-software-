6332487781d7d55f05fb71935ea3894aa102ecc7
Ruth <146713899+Ruthneemu@users.noreply.github.com>
2024-08-07 18:52:45 +0000

    all staged files

--- butchery.js
+++ butchery.js
@@ -60,3 +60,27 @@ eyeIcon.addEventListener('touchstart', () => {
     eyeIcon.classList.remove('fa-eye-slash');
   }
 });
+/*google maps*/
+// Create the map
+document.addEventListener("DOMContentLoaded", function() {
+  const searchButton = document.getElementById("searchButton");
+  const saveButton = document.getElementById("saveButton");
+
+  searchButton.addEventListener("click", function() {
+    alert("Search button clicked!");
+  });
+
+  saveButton.addEventListener("click", function() {
+    alert("Save button clicked!");
+  });
+});
+  // Create the map
+  var map = L.map('map').setView([51.505, -0.09], 13);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    subdomains: ['a', 'b', 'c'],
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
