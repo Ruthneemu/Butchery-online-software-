f2fa585bf71311f58dc089e0dc84f70454c5d718
Ruth <146713899+Ruthneemu@users.noreply.github.com>
2025-05-18 01:19:31 +0300

    updated files

--- docs/admin/supabaseClient.js
+++ docs/admin/supabaseClient.js
@@ -1,5 +1,9 @@
-import { createClient} from '@supabase/supabase-js';
+// supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://pqfofbniphvkkdscpdem.supabase.co";
-const supabaseKey = peyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxZm9mYm5pcGh2a2tkc2NwZGVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0OTIzODIsImV4cCI6MjA2MzA2ODM4Mn0.YhA7wXqI5-b2YM9A5QPKHf13nhx2fTj9PotVT_2Q92Q;
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxZm9mYm5pcGh2a2tkc2NwZGVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0OTIzODIsImV4cCI6MjA2MzA2ODM4Mn0.YhA7wXqI5-b2YM9A5QPKHf13nhx2fTj9PotVT_2Q92Q";
+
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase; // ✅ Now you can import it as default
