86f6dd42fd6ea3a947ac5240e28050ec8c72fade
Ruth <146713899+Ruthneemu@users.noreply.github.com>
2025-05-19 15:44:52 +0300

    Update app.jsx and postcss.config.js

--- docs/admin/app.jsx
+++ docs/admin/app.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './src/style.css'; // ensure correct path to Tailwind CSS
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route 
+} from 'react-router-dom';
+import Dashboard from '.pages/dashboard';
+import Inventory from './pages/inventory';
+import Sales from './pages/sales';
+import Reports from './pages/reports';
 const App = () => {
   return (
-    <div className="p-4 text-center text-xl text-blue-600">
-           ✅ React is working!
-    </div>
+    <Router>
+      <Routes>
+        <Routine path="/" element={<Dashboard />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/sales" element={<Sales />} />
+        <Route path="/reports" element={<Reports />} />
+      </Routes>
+    </Router>
   );
 };
 
